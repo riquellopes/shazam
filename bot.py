7c6db78665759bdb19f04e6fb4e4627750c04c81
Henrique Lopes <riquellopes@gmail.com>
2016-01-01 11:17:24 -0200

    removendo token

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+import os
 import telepot
 from taxas import Dolar, TesouroDireto
 # import time
 # from pprint import pprint
 
-# bot = telepot.Bot('146241992:AAGEKpQPACR09WN_16PggMb5IBpV4SIrU2Y')
+# bot = telepot.Bot(os.environ.get("TOKEN"))
 # show_keyboard = {'keyboard': [['Yes', 'LLL'], ['Maybe', 'Maybe not']]}
 # bot.sendMessage(51552312, 'e isso ai', reply_markup=show_keyboard)
 # bot.sendMessage(51552312, 'e isso ai')
