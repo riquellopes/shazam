c7a03207bd96b8e0c5f738568f3ffbac3e84ff07
Henrique Lopes <riquellopes@gmail.com>
2016-01-02 12:50:45 -0200

    refatorando test

--- taxas/tests/test_cdi.py
+++ taxas/tests/test_cdi.py
@@ -1,20 +1,7 @@
-from os import path
-PROJECT_ROOT = path.abspath(path.dirname(__file__))
-
 import responses
 import pytest
 from taxas import Cdi
-
-
-class MockResponse:
-
-    def __init__(self, file_test):
-        self.file_test = path.join(PROJECT_ROOT, file_test)
-
-    def __str__(self):
-        handle = open(self.file_test)
-        html = "".join(handle)
-        return html
+from . import MockResponse
 
 
 @responses.activate
